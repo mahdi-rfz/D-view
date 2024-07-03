26f5b2cbc1310663e1cca9b4c9c43f1331d04b52
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-03 15:19:01 +0330

    add help comment

--- configs/config.py
+++ configs/config.py
@@ -1,6 +1,10 @@
 api_url = ("http://127.0.0.1:9887/d_view")
-check_time = (30) #min
-email = ("")
+check_time = (60) #min              type : int
+"""
+The time that passes between sending two emails
+"""
+
+email = ("") #receiver email
 
 #-----------------------------------------------
 temp_file_address = ("") #find /sys -type f -name 'temp*'
