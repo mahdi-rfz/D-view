17a28007d3f5cb4ec36fe680e4d6f7a16ac9e632
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-04 19:08:13 +0330

    add help comment

--- client/client_config.py
+++ client/client_config.py
@@ -1,5 +1,11 @@
 
+#This IP must be equal to the local or global IP address to 
+# be used in the LAN network with the Internet
 api_url = ("http://127.0.0.1:9887/d_view")
+
+
 #--------------------------------------------------
+ #This user name and token must be equal to /d-view/configs/user_token.py
+ # to be used inside the Internet
 username = ("user")
 token = ("ealkcjveafgDAtg")
